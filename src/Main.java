1bf81939046af5e6a2cf3bed064bad96e40967cd
SaraRivera-p <sarariverapalacios@gmail.com>
2025-11-24 09:31:13 -0500

    no se q decir

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import gui.MainWindow;
+
 
 public class Main {
     public static void main(String[] args) {
--- src/services/VentaService.java
+++ src/services/VentaService.java
@@ -1,4 +1,3 @@
-import models.*;
 import java.util.ArrayList;
 
 public class VentaService {
